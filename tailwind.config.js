4fc03705d10d724e4dbd9ebda65d0bae11a8ff74
linhuan <1315558594@qq.com>
2025-11-21 03:03:26 +0800

    #fix 修改了tailwind配置和index.html

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,9 @@
 /** @type {import('tailwindcss').Config} */
-// eslint-disable-next-line no-undef
+
 module.exports = {
   darkMode: 'class',
   content: ['./index.html', './src/**/*.{vue,js,ts,jsx,tsx}'],
-   
+
   theme: {
     extend: {
       animation: {
